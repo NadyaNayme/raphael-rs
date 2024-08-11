d61c30d8b6a9ae7a573bcbb88c662d02e0466355
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2024-08-11 14:53:18 -0700

    test

--- solvers/src/utils/pareto_front_builder.rs
+++ solvers/src/utils/pareto_front_builder.rs
@@ -170,7 +170,7 @@ where
         };
 
         self.segments.push(segment_c);
-        self.buffer.truncate(segment_c.offset + segment_c.length);
+        self.buffer.truncate(segment_c.offset + segment_c.length).shrink_to_fit();
     }
 
     pub fn peek(&mut self) -> Option<Box<[ParetoValue<T, U>]>> {
